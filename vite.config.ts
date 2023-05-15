92cd2996ebf14eac10946fba22bb6750b88c4748
Max Bronner <max2bronner@gmail.com>
2023-05-15 23:48:55 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
+  },
+  plugins: [
+    ViteMinifyPlugin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      minifyURLs: true,
+      removeComments: true,
+      removeRedundantAttributes: true,
+      removeEmptyAttributes: true,
+    }),
+  ],
+});
