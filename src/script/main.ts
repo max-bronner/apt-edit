fb7cdd8b3f2115a816b36beef1ad8ac1fa9e79b2
Max Bronner <max2bronner@gmail.com>
2023-05-16 23:47:12 +0200

    Add file reader

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,1 +1,17 @@
+import { parseConst } from './parser/constParser';
 
+const fileInput= <HTMLInputElement>document.getElementById('file-reader__input');
+
+fileInput.addEventListener('change', async function(event: Event) {
+  
+  const target = <HTMLInputElement>event.target;
+
+  const constFile  =  target.files && target.files[0];
+
+  if (constFile) {
+    const constData = await constFile.arrayBuffer().then((arrayBuffer: ArrayBuffer) => {
+      return parseConst(arrayBuffer);
+    });
+    console.log('const', constData);
+  }
+}, false);
