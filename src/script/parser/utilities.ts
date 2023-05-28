216b8528d097480ed9f7cdfe38316c02741fd934
Max Bronner <max2bronner@gmail.com>
2023-05-28 11:41:08 +0200

    Remove unused utilities

--- src/script/parser/utilities.ts
+++ src/script/parser/utilities.ts
@@ -3,17 +3,6 @@ export const enum FileType {
   Const = 'const',
 }
 
-const decoder = new TextDecoder();
-
-export const isFalsy = (element: any): boolean => !element;
-
-export const parseString = (buffer: ArrayBuffer, index: number = 0): string => {
-  const charArray = new Uint8Array(buffer, index);
-  const length = charArray.findIndex(isFalsy);
-  const stringArray = charArray.subarray(0, length);
-  return decoder.decode(stringArray);
-};
-
 export const getFiles = (input: FileList | null): File[] => {
   if (input?.length !== 2) {
     throw new Error(
