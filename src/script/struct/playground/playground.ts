263990e73b1190297021f8a66525087d308642bc
Max Bronner <max2bronner@gmail.com>
2023-06-04 21:39:54 +0200

    Add apt parsing to playground

--- src/script/struct/playground/playground.ts
+++ src/script/struct/playground/playground.ts
@@ -18,6 +18,36 @@ constStruct.addMember('itemCount').uint32();
 constStruct.addMember('unknown').uint32();
 constStruct.addMember('items').array(constItems, 'itemCount');
 
+const headerStruct = createStruct();
+headerStruct.addMember('fileType').string();
+
+const importStruct = createStruct();
+importStruct.addMember('movie').pointer().string();
+importStruct.addMember('name').pointer().string();
+importStruct.addMember('character').uint32();
+importStruct.addMember('pointer').uint32();
+
+const exportStruct = createStruct();
+exportStruct.addMember('name').pointer().string();
+exportStruct.addMember('character').uint32();
+
+const outputMovieStruct = createStruct();
+outputMovieStruct.addMember('type').uint32();
+outputMovieStruct.addMember('signature').uint32();
+outputMovieStruct.addMember('frameCount').uint32();
+outputMovieStruct.addMember('frames').uint32();
+outputMovieStruct.addMember('pointer').uint32();
+outputMovieStruct.addMember('characterCount').uint32();
+outputMovieStruct.addMember('characters').pointer();
+outputMovieStruct.addMember('screenSizeX').uint32();
+outputMovieStruct.addMember('screenSizeY').uint32();
+outputMovieStruct.addMember('unknown').uint32();
+outputMovieStruct.addMember('importCount').uint32();
+outputMovieStruct.addMember('imports').pointer().array(importStruct, 'importCount');
+outputMovieStruct.addMember('exportCount').uint32();
+outputMovieStruct.addMember('exports').pointer().array(exportStruct, 'exportCount');
+outputMovieStruct.addMember('count').uint32();
+
 export const playground = async () => {
   const viewConst = new DataView(
     await fetch('script/struct/playground/example.const').then((resp) => resp.arrayBuffer()),
@@ -25,4 +55,5 @@ export const playground = async () => {
   const viewApt = new DataView(await fetch('script/struct/playground/example.apt').then((resp) => resp.arrayBuffer()));
 
   const dataConst = constStruct.parse(viewConst);
+  const dataApt = outputMovieStruct.parse(viewApt, dataConst.aptOffset as unknown as number);
 };
