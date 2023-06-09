5acfa3ce5edf0042e287503c2e6ce0897eccf923
Max Bronner <max2bronner@gmail.com>
2023-06-09 20:05:45 +0200

    Add test for output frames

--- src/script/struct/playground/playground.ts
+++ src/script/struct/playground/playground.ts
@@ -7,11 +7,6 @@ const parseConstItemType: CustomCallback = (view, offset, data) => {
   return data.type === 1 ? getString(view, dataValue) : dataValue;
 };
 
-const parseCharacterType: CustomCallback = (view, offset, data) => {
-  if (offset === 0) return 0;
-  return view.getUint32(offset, true);
-};
-
 const constItems = createStruct();
 constItems.addMember('type').uint32();
 constItems.addMember('value').custom(parseConstItemType, 4);
@@ -30,8 +25,49 @@ const characterStruct = createStruct();
 characterStruct.addMember('type').uint32();
 characterStruct.addMember('signature').uint32();
 
+const vector2Struct = createStruct();
+vector2Struct.addMember('X').uint32();
+vector2Struct.addMember('Y').uint32();
+
+const transformStruct = createStruct();
+transformStruct.addMember('m00').uint32();
+transformStruct.addMember('m01').uint32();
+transformStruct.addMember('m10').uint32();
+transformStruct.addMember('m11').uint32();
+
 const frameItemStruct = createStruct();
 frameItemStruct.addMember('type').uint32();
+
+const outputActionStruct = createStruct(frameItemStruct);
+outputActionStruct.addMember('actionBytes').pointer();
+
+const frameLabelStruct = createStruct(frameItemStruct);
+frameLabelStruct.addMember('label').pointer().string();
+frameLabelStruct.addMember('flags').uint32();
+frameLabelStruct.addMember('frame').uint32();
+
+const placeObjectStruct = createStruct(frameItemStruct);
+placeObjectStruct.addMember('flags').uint32();
+placeObjectStruct.addMember('depth').uint32();
+placeObjectStruct.addMember('character').uint32();
+placeObjectStruct.addMember('rotateAndScale').struct(transformStruct);
+placeObjectStruct.addMember('translate').struct(vector2Struct);
+placeObjectStruct.addMember('colorTransform').uint32();
+placeObjectStruct.addMember('unknown').uint32();
+placeObjectStruct.addMember('ratio').uint32();
+placeObjectStruct.addMember('name').pointer().string();
+placeObjectStruct.addMember('clipdepth').uint32();
+placeObjectStruct.addMember('placeObjectActions').pointer();
+
+const removeObjectStruct = createStruct(frameItemStruct);
+removeObjectStruct.addMember('depth').uint32();
+
+const backgroundColorStruct = createStruct(frameItemStruct);
+backgroundColorStruct.addMember('color').uint32();
+
+const initActionStruct = createStruct(frameItemStruct);
+initActionStruct.addMember('sprite').uint32();
+initActionStruct.addMember('actionBytes').uint32();
 
 const outputFrameStruct = createStruct();
 outputFrameStruct.addMember('frameItemCount').uint32();
@@ -52,7 +88,7 @@ outputMovieStruct.addMember('frameCount').uint32();
 outputMovieStruct.addMember('frames').pointer().arrayAlt('frameCount').struct(outputFrameStruct);
 outputMovieStruct.addMember('pointer').uint32();
 outputMovieStruct.addMember('characterCount').uint32();
-outputMovieStruct.addMember('characters').pointer().arrayAlt('characterCount').pointer().custom(parseCharacterType, 4);
+outputMovieStruct.addMember('characters').pointer().arrayAlt('characterCount').pointer().uint32();
 outputMovieStruct.addMember('screenSizeX').uint32();
 outputMovieStruct.addMember('screenSizeY').uint32();
 outputMovieStruct.addMember('unknown').uint32();
@@ -62,6 +98,15 @@ outputMovieStruct.addMember('exportCount').uint32();
 outputMovieStruct.addMember('exports').pointer().arrayAlt('exportCount').struct(exportStruct);
 outputMovieStruct.addMember('count').uint32();
 
+enum FrameItemType {
+  Action = 1,
+  FrameLabel = 2,
+  PlaceObject = 3,
+  RemoveObject = 4,
+  BackgroundColor = 5,
+  InitAction = 8,
+}
+
 export const playground = async () => {
   const viewConst = new DataView(
     await fetch('script/struct/playground/example.const').then((resp) => resp.arrayBuffer()),
@@ -70,6 +115,26 @@ export const playground = async () => {
 
   const dataConst = constStruct.parse(viewConst);
   const dataApt = outputMovieStruct.parse(viewApt, dataConst.aptOffset as unknown as number);
+
+  const dataOutputFrames = dataApt.frames.map((frames) =>
+    frames.frameItems?.map((frameItem: number) => {
+      const type = viewApt.getUint32(frameItem, true);
+      switch (type) {
+        case FrameItemType.Action:
+          return outputActionStruct.parse(viewApt, frameItem);
+        case FrameItemType.FrameLabel:
+          return frameLabelStruct.parse(viewApt, frameItem);
+        case FrameItemType.PlaceObject:
+          return placeObjectStruct.parse(viewApt, frameItem);
+        case FrameItemType.RemoveObject:
+          return removeObjectStruct.parse(viewApt, frameItem);
+        case FrameItemType.BackgroundColor:
+          return backgroundColorStruct.parse(viewApt, frameItem);
+        case FrameItemType.InitAction:
+          return initActionStruct.parse(viewApt, frameItem);
+      }
+    }),
+  );
 
   console.log(dataApt);
 };
